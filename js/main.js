db174047fe863a1d8a89bb1850b1717f747dd44c
Dax2976 <ferrodavide55@gmail.com>
2022-03-25 17:21:56 +0100

    final commit + made with love dax

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 
 let km = document.getElementById('kmInput')
 let nome = document.getElementById('nomeInput')
-let age = document.getElementById('ageLista')
+let eta = document.getElementById('ageList')
 let btngenera = document.getElementById('genera')
+let display = document.getElementById('Dnone')
 
 
 // inizio calcoli
@@ -11,19 +12,37 @@ let btngenera = document.getElementById('genera')
 btngenera.addEventListener('click',function(){
     nome = nome.value
     km = km.value
-    age = age.value
+    eta = eta.value
 
 
     let costoTreno = 0.21
     let bigliettoIntero = km*costoTreno
-    let scontoMinori = costoTreno - (costoTreno*0.2)
-    let scontoOver = costoTreno - (costoTreno*0.4)
+    let scontoMinori = bigliettoIntero - (bigliettoIntero*0.2)
+    let scontoOver = bigliettoIntero - (bigliettoIntero*0.4)
 
     let prezzoGiovani = bigliettoIntero - scontoMinori
     let prezzoOver = bigliettoIntero - scontoOver
 
-    bigliettoIntero = Math.round(bigliettoIntero * 10) / 10;
-    prezzoGiovani= Math.round(prezzoGiovani * 10) / 10;
-    prezzoOver = Math.round(prezzoOver * 10) / 10;
+    bigliettoIntero = Math.round(bigliettoIntero * 10) / 10
+    prezzoGiovani= Math.round(prezzoGiovani * 10) / 10
+    prezzoOver = Math.round(prezzoOver * 10) / 10
+    
+    // console.log(typeof (scontoOver))
+    display.classList.add('dBlock')
 
+    document.getElementById('nomeUtente').innerHTML = nome
+
+    if(eta == "maggiorenne") {
+        document.getElementById("offerta").innerHTML = `Biglietto Standard`
+        document.getElementById("ticketPrice").innerHTML = `€${bigliettoIntero}`
+    } else if(eta == "minorenne"){
+        document.getElementById("offerta").innerHTML = `Biglietto Minori`
+        document.getElementById("ticketPrice").innerHTML = `€${scontoMinori}`
+    } else{eta == "over65"
+        document.getElementById("offerta").innerHTML = `Biglietto Anziani`
+        document.getElementById("ticketPrice").innerHTML = `€${scontoOver}`
+    }
+
+    document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 100)
+    document.getElementById('codeCp').innerHTML = Math.floor(Math.random() * 100000)
 })
